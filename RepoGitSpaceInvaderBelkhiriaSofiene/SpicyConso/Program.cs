82b8b5a14d25265a1becd73e639c4751dac8bdf1
termimi <sofiene1023@gmail.com>
2023-10-06 10:11:05 +0200

    Changement de la stucture + ADD file WIP

---
+++ RepoGitSpaceInvaderBelkhiriaSofiene/SpicyConso/Program.cs
@@ -0,0 +1,92 @@
+﻿using Display;
+using Model;
+
+int clear = 8;
+uint frameNumber = 0; // Comptage du nombre d'images à affichées
+
+
+Playground.Init();// Initialisation de la config playground
+
+List<Missile> missiles = new List<Missile>();
+
+Alien invader = new Alien(0, 0, 30);
+Player player = new Player(Console.WindowWidth / 2 - 3, Console.WindowHeight - 3);
+
+
+
+while (true)
+{
+
+    // Action de l'utilisateur par apport a sa saisie
+    if (Console.KeyAvailable) //Si l'utilisateur presse une touche
+    {
+        ConsoleKeyInfo keyPressed = Console.ReadKey(true);
+        switch (keyPressed.Key)
+        {
+            case ConsoleKey.Escape:
+                Environment.Exit(0);
+                break;
+
+            case ConsoleKey.W:
+                player.moveUp();
+                break;
+
+            case ConsoleKey.S:
+                player.moveDown();
+                break;
+
+            case ConsoleKey.A:
+                player.moveLeft();
+                break;
+
+            case ConsoleKey.D:
+                player.moveRight();
+                break;
+
+            case ConsoleKey.Spacebar:
+                Missile missileDefault = new Missile(8);//Initialisation du missile de base
+                missiles.Add(missileDefault);
+                player.chargement(missileDefault);//On charge le missile dans le joueeur
+                player.dropMissile();//On lance le missile
+                break;
+        }
+    }
+
+    if (frameNumber % clear == 0)
+    {
+        Playground.Clear();
+        Playground.DrawPlayer(player);//Affichage du joueur
+        Playground.DrawAlien(invader);//Affichage de l'alien
+
+        foreach (Missile missileDefault in missiles)
+        {
+            Playground.DrawMissile(missileDefault);// Affichage du missile
+        }
+    }
+
+    frameNumber++;
+
+    if (frameNumber % 3 == 0)//Vitesse de l'invaders
+    {
+        invader.moveRight();//Déplacement alien droite
+        invader.moveLeft();//Déplacement alien gauche
+    }
+
+    for (int i = missiles.Count - 1; i >= 0; i--)//Boucle qui parcours ma liste de missile a l'envers 
+    {
+        Missile missileDefault = missiles[i];
+
+        if (frameNumber % 2 == 0) // Vitesse du missile
+        {
+            missileDefault.UpdateMisille(); // Déplacement du missile
+            if (!missileDefault.missileIsLaunched)
+            {
+                missiles.RemoveAt(i);
+            }
+        }
+    }
+
+    //Temp d'attente
+    Thread.Sleep(3);
+}
+ 
--- RepoGitSpaceInvaderBelkhiriaSofiene/SpicyConso/SpicyConso/Program.cs
+++ RepoGitSpaceInvaderBelkhiriaSofiene/SpicyConso/SpicyConso/Program.cs
@@ -1,92 +1,292 @@
 ﻿using Display;
 using Model;
-
-int clear = 8;
-uint frameNumber = 0; // Comptage du nombre d'images à affichées
-
-
-Playground.Init();// Initialisation de la config playground
-
-List<Missile> missiles = new List<Missile>();
-
-Alien invader = new Alien(0, 0, 30);
-Player player = new Player(Console.WindowWidth / 2 - 3, Console.WindowHeight - 3);
-
-
+using Storage;
 
 while (true)
 {
+    Console.Clear();
+    uint frameNumber = 0; // count the number of frames displayed
+    int clearInterval = 8;
+    bool spawn = true;
+    int manche = 1;
 
-    // Action de l'utilisateur par apport a sa saisie
-    if (Console.KeyAvailable) //Si l'utilisateur presse une touche
+
+
+    List<Alien> enemi = new List<Alien>();
+    Player player1 = new Player(76, Playground.SCREEN_HEIGHT - 4);
+    List<MissilePlayer> missilePlayer = new List<MissilePlayer>();
+    List<MissileAlien> missileAliens = new List<MissileAlien>();
+    Menu menu = new Menu();
+    GameOverMenu gameOver = new GameOverMenu();
+
+    ConsoleKeyInfo keyPressed = Console.ReadKey(true);
+    Playground.Init();
+
+    do
     {
-        ConsoleKeyInfo keyPressed = Console.ReadKey(true);
-        switch (keyPressed.Key)
+        if (Console.KeyAvailable)
         {
-            case ConsoleKey.Escape:
-                Environment.Exit(0);
-                break;
-
-            case ConsoleKey.W:
-                player.moveUp();
-                break;
-
-            case ConsoleKey.S:
-                player.moveDown();
-                break;
-
-            case ConsoleKey.A:
-                player.moveLeft();
-                break;
-
-            case ConsoleKey.D:
-                player.moveRight();
-                break;
-
-            case ConsoleKey.Spacebar:
-                Missile missileDefault = new Missile(8);//Initialisation du missile de base
-                missiles.Add(missileDefault);
-                player.chargement(missileDefault);//On charge le missile dans le joueeur
-                player.dropMissile();//On lance le missile
-                break;
-        }
-    }
-
-    if (frameNumber % clear == 0)
-    {
-        Playground.Clear();
-        Playground.DrawPlayer(player);//Affichage du joueur
-        Playground.DrawAlien(invader);//Affichage de l'alien
-
-        foreach (Missile missileDefault in missiles)
-        {
-            Playground.DrawMissile(missileDefault);// Affichage du missile
-        }
-    }
-
-    frameNumber++;
-
-    if (frameNumber % 3 == 0)//Vitesse de l'invaders
-    {
-        invader.moveRight();//Déplacement alien droite
-        invader.moveLeft();//Déplacement alien gauche
-    }
-
-    for (int i = missiles.Count - 1; i >= 0; i--)//Boucle qui parcours ma liste de missile a l'envers 
-    {
-        Missile missileDefault = missiles[i];
-
-        if (frameNumber % 2 == 0) // Vitesse du missile
-        {
-            missileDefault.UpdateMisille(); // Déplacement du missile
-            if (!missileDefault.missileIsLaunched)
+            keyPressed = Console.ReadKey(true);
+            switch (keyPressed.Key)
             {
-                missiles.RemoveAt(i);
+                case ConsoleKey.W:
+                    menu.choix1--;
+                    if (menu.choix1 < 0)
+                    {
+                        menu.choix1 = 1;
+                    }
+                    break;
+                case ConsoleKey.S:
+                    menu.choix1++;
+                    if (menu.choix1 > 1)
+                    {
+                        menu.choix1 = 0;
+                    }
+                    break;
+
+            }
+        }
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        menu.DrawTitle();
+        menu.DrawJouer();
+        menu.DrawClassemnt();
+        if (menu.choix1 == 0)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            menu.DrawJouer();
+        }
+        if (menu.choix1 == 1)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            menu.DrawClassemnt();
+
+        }
+        if (keyPressed.Key == ConsoleKey.Enter && menu.choix1 == 0)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            break; // Sort de la boucle si Enter est pressé et le choix vaut 0
+        }
+    } while (true);
+    if (player1.playerDead)
+    {
+        while (true)
+        {
+            gameOver.DrawTitle();
+        }
+    }
+
+    while (true)
+    {
+        if (spawn)
+        {
+            int c = 0;
+            int x;
+            int y = 0;
+            for (int i = 0; i < manche * 3; i++)
+            {
+                x = i * 13;
+                if (i > 9)
+                {
+                    x = c * 13;
+                    c++;
+                    if(i%10 ==0)
+                    {
+                        y += 10;
+                    }
+                }
+                Alien alain = new Alien(x, y);
+                enemi.Add(alain);
+            }
+        }
+        spawn = false;
+
+        // Actions de l'utilisateur
+        if (Console.KeyAvailable) // L'utilisateur a pressé une touche
+        {
+            keyPressed = Console.ReadKey(true);
+            switch (keyPressed.Key)
+            {
+                case ConsoleKey.Escape:
+                    Environment.Exit(0);
+                    break;
+                case ConsoleKey.D:
+                    player1.moveRight();
+                    break;
+                case ConsoleKey.A:
+                    player1.moveLeft();
+                    break;
+                case ConsoleKey.W:
+                    player1.moveUP();
+                    break;
+                case ConsoleKey.S:
+                    player1.moveDown();
+                    break;
+                case ConsoleKey.Spacebar:
+                    if (frameNumber % 1 == 0)
+                    {
+                        MissilePlayer munitionNormal = new MissilePlayer(player1);
+                        missilePlayer.Add(munitionNormal);
+                        player1.Chargement(munitionNormal);
+                        player1.MisilleLaunch(munitionNormal);
+                    }
+                    break;
+
+            }
+        }
+        // Affichage
+        if (frameNumber % 5 == 0)
+        {
+            Playground.Clear();
+            Playground.DrawInGameMenu();
+            Playground.DrawScoreInGameMenu(Store.score);
+            Playground.DrawMancheInGameMenu(manche);
+            foreach (Alien alain in enemi)
+            {
+                Playground.DrawAlien(alain);
+                Playground.DrawDeadAlien(alain);
+            }
+            Playground.DrawPlayer(player1);
+            Playground.DrawPlayerDead(player1);
+            foreach (MissilePlayer munitionNormal in missilePlayer)
+            {
+                Playground.DrawLaunchMissile(munitionNormal);
+            }
+            foreach (MissileAlien munitionDefaut in missileAliens)
+            {
+                //foreach (Alien alain in enemi)
+                Playground.DrawLaunchMissileAlien(munitionDefaut);
+            }
+        }
+
+        frameNumber++;
+
+        // Déplacement au niveau du modèle (état)
+        if (frameNumber % 3 == 0)
+        {
+            foreach (Alien alain in enemi)
+            {
+                alain.MoveRight();
+                alain.moveLeft();
+            }
+        }
+        Random randomSpeedFire = new Random();
+        if (frameNumber % 65 == 0)
+        {
+            foreach (Alien alain in enemi)
+            {
+
+                int speedFire = randomSpeedFire.Next(5);
+                if (speedFire % 2 == 0)
+                {
+                    MissileAlien munitionDefaut = new MissileAlien(alain);
+                    missileAliens.Add(munitionDefaut);
+                    alain.ChargementAlien(munitionDefaut);
+                    alain.MisilleLaunchAlien(munitionDefaut);
+                }
+
+            }
+        }
+        foreach (MissilePlayer munitionNormal in missilePlayer)
+        {
+            if (frameNumber % 2 == 0)
+                munitionNormal.MissileUpdate();
+            foreach (Alien alain in enemi)
+            {
+                alain.AlienTouched(munitionNormal);
+            }
+            for (int i = enemi.Count - 1; i >= 0; i--)
+            {
+                Alien alain = enemi[i];
+                if (alain.alienDead && alain.deadDrawPassed)
+                {
+                    Store.score += 100;
+                    enemi.RemoveAt(i);
+                }
+                if (enemi.Count == 0)
+                {
+                    manche++;
+                    spawn = true;
+                }
+            }
+        }
+        for(int i = missilePlayer.Count -1; i>=0; i--)
+        {
+            MissilePlayer munitionNormal1 = missilePlayer[i];
+            if (munitionNormal1.missileTouched)
+            {
+                missilePlayer.RemoveAt(i);
+            }
+        }
+        foreach (MissileAlien munitionDefaut in missileAliens)
+        {
+            if (frameNumber % 5 == 0)
+                munitionDefaut.MissileUpdate();
+            player1.PlayerTouched(munitionDefaut);
+
+        }
+        for (int i = missileAliens.Count - 1; i >= 0; i--)
+        {
+            MissileAlien munitionDefaut1 = missileAliens[i];
+            if (munitionDefaut1.missileY == Console.WindowHeight)
+            {
+
+                missileAliens.RemoveAt(i);
+            }
+            if(munitionDefaut1.missileTouched)
+                missileAliens.RemoveAt(i);
+        }
+
+        // Autosave
+        if (frameNumber % 1000 == 0)
+        {
+            foreach (Alien alain in enemi)
+                Store.StoreAlien(alain);
+        }
+            
+
+        // Timing
+        Thread.Sleep(3);
+
+
+        if (player1.playerDead)
+        {
+            Console.Clear();
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    keyPressed = Console.ReadKey(true);
+                    switch (keyPressed.Key)
+                    {
+                        case ConsoleKey.W:
+                            menu.choix1--;
+                            if (gameOver.choix1 < 0)
+                            {
+                                gameOver.choix1 = 1;
+                            }
+                            break;
+                        case ConsoleKey.S:
+                            gameOver.choix1++;
+                            if (gameOver.choix1 > 1)
+                            {
+                                gameOver.choix1 = 0;
+                            }
+                            break;
+                            
+
+                    }
+                }
+                Console.ForegroundColor = ConsoleColor.Red;
+                gameOver.DrawTitle();
+                Console.SetCursorPosition((Console.WindowWidth / 3), 25);
+                Console.Write($"Votre score: {Store.score}");
+                if (keyPressed.Key == ConsoleKey.Enter && menu.choix1 == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break; // Sort de la boucle si Enter est pressé et le choix vaut 0
+                }
             }
         }
     }
-
-    //Temp d'attente
-    Thread.Sleep(3);
 }
- 
